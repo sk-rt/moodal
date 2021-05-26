1a7c1e51463565853cc0a60d9db6cab6f7579f80
Ryuta Sakai <info@sakairyuta.com>
2021-05-26 13:33:13 +0900

    feat: update packages in docs

--- docs/src/js/main.js
+++ docs/src/js/main.js
@@ -5,77 +5,77 @@ import axios from 'axios';
 import Moodal from 'moodal';
 
 document.addEventListener(
-    'DOMContentLoaded',
-    () => {
-        modalInit();
-    },
-    false
+  'DOMContentLoaded',
+  () => {
+    modalInit();
+  },
+  false
 );
 
 const modalInit = () => {
-    const modal = new Moodal('.c-moodal', {
-        noBackgroundScroll: true,
-        logLevel: 3
-    });
-    if (!modal.isValid) {
+  const modal = new Moodal('.c-moodal', {
+    noBackgroundScroll: true,
+    logLevel: 3,
+  });
+  if (!modal.isValid) {
+    return;
+  }
+
+  const modalCtrl = modal.addController({
+    controllerAttr: 'data-modal-target',
+    getContent: (arg) => {
+      const wrapper = document.getElementById(arg);
+      if (!wrapper) {
         return;
-    }
+      }
+      const content = document.createElement('div');
+      content.innerHTML = wrapper.innerHTML;
+      return content;
+    },
+  });
 
-    const modalCtrl = modal.addController({
-        controllerAttr: 'data-modal-target',
-        getContent: arg => {
-            const wrapper = document.getElementById(arg);
-            if (!wrapper) {
-                return;
+  modalCtrl.show('aaa');
+
+  modal.addController({
+    controllerAttr: 'data-modal-acync',
+    waitContentLoaded: true,
+    getContent: (trigger) => {
+      return new Promise((resolve, rejects) => {
+        (async () => {
+          try {
+            const res = await axios.get(trigger, {
+              responseType: 'document',
+            });
+            const content = res.data.querySelector('#content');
+            if (content) {
+              resolve(content);
+            } else {
+              throw new Error('No Content!');
             }
-            const content = document.createElement('div');
-            content.innerHTML = wrapper.innerHTML;
-            return content;
-        }
-    });
+          } catch (error) {
+            rejects(error);
+          }
+        })();
+      });
+    },
+    beforeAppend: (context) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          // eslint-disable-next-line no-console
+          console.log('before:append', context);
+          resolve(context);
+        }, 100);
+      });
+    },
 
-    modalCtrl.show('aaa');
-
-    modal.addController({
-        controllerAttr: 'data-modal-acync',
-        waitContentLoaded: true,
-        getContent: trigger => {
-            return new Promise((resolve, rejects) => {
-                (async () => {
-                    try {
-                        const res = await axios.get(trigger, {
-                            responseType: 'document'
-                        });
-                        const content = res.data.querySelector('#content');
-                        if (content) {
-                            resolve(content);
-                        } else {
-                            throw new Error('No Content!');
-                        }
-                    } catch (error) {
-                        rejects(error);
-                    }
-                })();
-            });
-        },
-        beforeAppend: context => {
-            return new Promise(resolve => {
-                setTimeout(() => {
-                    // eslint-disable-next-line no-console
-                    console.log('before:append', context);
-                    resolve(context);
-                }, 100);
-            });
-        },
-
-        beforeShow: ({ content }) => {
-            // eslint-disable-next-line no-console
-            console.log('before:show');
-            content.querySelector('img').classList.add('test');
-        },
-        beforeHide: () => {
-            // eslint-disable-next-line no-console
-            console.log('before:Hide');
-        }
-    });
+    beforeShow: ({ content }) => {
+      // eslint-disable-next-line no-console
+      console.log('before:show');
+      content.querySelector('img').classList.add('test');
+    },
+    beforeHide: () => {
+      // eslint-disable-next-line no-console
+      console.log('before:Hide');
+    },
+  });
 };
--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -2,86 +2,83 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const publicPath = '';
-let outputPath, devMode, sourceMap;
-if (process.env.NODE_ENV === 'production') {
-    outputPath = `${__dirname}/dist/${publicPath}`;
-    devMode = 'production';
-    sourceMap = false;
-} else {
-    outputPath = `${__dirname}/public/${publicPath}`;
-    devMode = 'development';
-    sourceMap = true;
-}
+const environment = process.env.NODE_ENV || 'development';
+const isDevelopment = environment === 'development';
+
 module.exports = {
-    entry: {
-        main: `${__dirname}/src/js/main.js`
-    },
-    target: 'web',
-    mode: devMode,
-    devtool: sourceMap ? 'inline-source-map' : false,
-    output: {
-        path: outputPath,
-        publicPath: publicPath,
-        filename: 'js/[name][hash].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
-            },
+  entry: {
+    main: `${__dirname}/src/js/main.js`,
+  },
+  target: 'web',
+  mode: isDevelopment ? environment : 'production',
+  devtool: isDevelopment ? 'inline-source-map' : false,
+  output: {
+    path: isDevelopment
+      ? `${__dirname}/public${publicPath}`
+      : `${__dirname}/dist${publicPath}`,
+    publicPath: publicPath,
+    filename: 'js/[name][hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
 
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: sourceMap,
-                            url: false,
-                            importLoaders: 2
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: sourceMap,
-                            plugins: [require('autoprefixer')]
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: sourceMap
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            ENV: JSON.stringify(devMode)
-        }),
-
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: isDevelopment,
+              url: false,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: isDevelopment,
+              postcssOptions: {
+                plugins: [require('autoprefixer')],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
     ],
-    devServer: {
-        contentBase: `${__dirname}/public`,
-        watchContentBase: true,
-        open: true,
-        host: '0.0.0.0',
-        useLocalIp: true
-    }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify(environment),
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: `${__dirname}/public`,
+    watchContentBase: true,
+    open: true,
+    host: '0.0.0.0',
+    useLocalIp: true,
+  },
 };
